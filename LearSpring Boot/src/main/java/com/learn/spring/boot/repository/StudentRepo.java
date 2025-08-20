13556acc7559449be70eb745b7411bd2c0a4ae07
Chand Mandol <chand241-15-157@diu.edu.bd>
2025-08-21 03:24:59 +0600

    Delete LearSpring Boot directory

--- LearSpring Boot/src/main/java/com/learn/spring/boot/repository/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.learn.spring.boot.repository;
-
-import com.learn.spring.boot.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
